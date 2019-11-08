3fcfcb484ffa4c6594651178a1114d58b9f724e2
lyn <1306292990@qq.com>
2019-11-08 23:08:20 +0800

    新增index

---
+++ src/app/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div></div>
+</template>
+
+<script type="text/ecmascript-6">
+        export default {};
+</script>
+
+<style lang="less" type="text/less" rel="stylesheet/less" scoped>
+
+</style>
